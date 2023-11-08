46595cf46811994e6d6e7f08f5b8a27dd68a29e9
Luíza Costa <117786812+luizamc003@users.noreply.github.com>
2023-11-08 10:02:12 -0300

    operações protegidas

--- Avaliacao.h
+++ Avaliacao.h
@@ -66,7 +66,7 @@ public:
                 case '#': // raiz quadrada
                     if (val1 < 0)
                     {
-                        resultado = val1;
+                        resultado = 0; // operacao protegida
                     }
                     else
                     {
@@ -82,7 +82,7 @@ public:
                 case '!':
                     if (val1 <= 0)
                     {
-                        resultado = val1;
+                        resultado = 0; // operacao protegida
                     }
                     else
                     {
