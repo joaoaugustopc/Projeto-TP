43f52cea6d91ded488175bd6876fef785685e65b
joaoaugustopc <joaoaugusto.jf@gmail.com>
2023-12-07 00:54:58 -0300

    aumentando numero de arvores obtidas em execucoes independentes

--- main.cpp
+++ main.cpp
@@ -265,7 +265,7 @@ int main()
     double time_spent = 0;
     clock_t begin = clock();
 
-    srand(9);
+    srand(7132);
     Arv **PopulacaoInicial = new Arv *[FAMILIA];
     Arv **PopulacaoGenitores = new Arv *[FAMILIA];
 
